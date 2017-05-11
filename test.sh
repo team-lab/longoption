866385b1c6a62a6d4e41a46ffbcd1c8d1952f24d
nazoking <nazoking@gmail.com>
2017-05-11 11:31:48 +0900

    fix test

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ set -e -x
 cd $(dirname $0)
 /bin/bash --version
 ./test_longoption.sh
-./test_README.sh 7 ./README.md
-./test_README.sh 9 ./README.ja.md
+./test_README.sh 11 ./README.md
+./test_README.sh 11 ./README.ja.md
  
